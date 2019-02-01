743dff9a584f9a1262434053c9f94ec96603cef5
USER <USER@10.52.33.237>
2019-02-01 14:34:29 +0300

    LabTwoTasks3-113453

---
+++ src/labtwotasks3/LabTwoTasks3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labtwotasks3;
+
+/**
+ *
+ * @author USER
+ */
+public class LabTwoTasks3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=25;
+        int b=39;
+        
+        if(a==b){
+            System.out.println("25 is equal to 39");}
+        else{
+            System.out.println("25 and 39 are not equal");}
+    }
+    
+}
